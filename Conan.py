afb3b903e90464185ebb142071586ebf7540b6c4
ChenSitong <1458634279@qq.com>
2019-05-20 21:18:22 +0800

    ggnb

--- Conan.py
+++ Conan.py
@@ -2,7 +2,22 @@ import struct
 
 
 def tamper(student_id):
-  pass
+  with open('lenna.bmp','r+b')as f:
+    id=54
+    for i in range(0,12):
+      if i==0:
+        id=id+int(student_id[i])*3
+        f.seek(id)
+        f.write(b'\x00\x00\x00')
+      else:
+        if int(student_id[i])==0:
+          id=27
+          f.seek(id,1)
+          f.write(b'\x00\x00\x00')
+        else:
+          id=(int(student_id[i])-1)*3
+          f.seek(id,1)
+          f.write(b'\x00\x00\x00')
 
 
 def detect():
